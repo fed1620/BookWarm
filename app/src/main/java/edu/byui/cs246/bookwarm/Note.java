926af7bb3c57360034454b51eca6f97520e47e1b
De5t1nY <pry12002@byui.edu>
2015-02-27 01:08:54 -0700

    I don't know why it complaints about inout/output method. It worked for the other project

---
+++ app/src/main/java/edu/byui/cs246/bookwarm/Note.java
@@ -0,0 +1,61 @@
+package edu.byui.cs246.bookwarm;
+
+import android.content.Context;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Megiddo on 2/26/2015.
+ */
+public class Note {
+    private String noteContent;
+    private Date dateAuthored;
+    private int pageNumber;
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public Date getDateAuthored() {
+        return dateAuthored;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setNoteContent(String noteContentTemp) {
+        noteContent = noteContentTemp;
+    }
+    public void setDateAuthored(Date dateAuthoredTemp) {
+        dateAuthored = dateAuthoredTemp;
+    }
+    public void setPageNumber(int pageNumberTemp) {
+        pageNumber = pageNumberTemp;
+    }
+    public void loadNoteFromFile() throws IOException {
+        try {
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(openFileInput("numbers")));
+            String inputString;
+            List<String> items = new ArrayList<>();
+            while ((inputString = inputReader.readLine()) != null) {
+
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void saveNoteToFile() {
+        try {
+            FileOutputStream fos = openFileOutput("note", Context.MODE_PRIVATE);
+
+            fos.close();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
