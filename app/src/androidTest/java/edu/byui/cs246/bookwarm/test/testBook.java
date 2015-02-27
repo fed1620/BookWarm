e3f291d0988c001560b931d91e9b4d5b35ea0346
PWNsbey <jake.ownsbey@gmail.com>
2015-02-27 16:33:18 -0700

    filled out the testBook class with two methods. Likely more to come.

--- app/src/androidTest/java/edu/byui/cs246/bookwarm/test/testBook.java
+++ app/src/androidTest/java/edu/byui/cs246/bookwarm/test/testBook.java
@@ -2,8 +2,25 @@ package edu.byui.cs246.bookwarm.test;
 
 import junit.framework.TestCase;
 
+import edu.byui.cs246.bookwarm.Book;
+
 /**
- * (INSERT DESCRIPTION HERE)
+ * Test the book class
  */
 public class testBook extends TestCase{
+
+    //check the title
+    public void testTitleStuff() {
+        Book dummyBook = new Book();
+        dummyBook.setTitle("Herp de Derp");
+
+        assertEquals("Herp de Derp", dummyBook.getTitle());
+    }
+
+    //void test with the book title
+    public void testBreakTheTitle () {
+        Book dummyBook = new Book();
+
+        assertEquals(null, dummyBook.getTitle());
+    }
 }
